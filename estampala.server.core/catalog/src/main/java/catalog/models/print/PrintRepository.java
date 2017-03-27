717c7ffadefba0f2f1f618d9e02d5c52cac7bc9b
JarveinR (akane) <jarvein.rivera@bizagi.com>
2017-03-27 13:57:43 -0500

    fix Print filter

--- estampala.server.core/catalog/src/main/java/catalog/models/print/PrintRepository.java
+++ estampala.server.core/catalog/src/main/java/catalog/models/print/PrintRepository.java
@@ -1,15 +1,11 @@
 package catalog.models.print;
 
 
-import java.util.Collection;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
 
 public interface PrintRepository extends JpaRepository<Print, UUID>, JpaSpecificationExecutor<Print> {
-    Collection<Print> findByName(String name);
 
-    Collection<Print> findAdvance(Theme theme, UUID owner, int initialRating, int finalRating, int initialPopularity, int finalPopularity);
 }
