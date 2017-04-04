7dad3a37956ab4f6052b8aa78b288070e5230e25
soybackend <josealvarez712@gmail.com>
2017-04-03 22:52:21 -0500

    listo el posho

--- web/app/models/shirt.js
+++ web/app/models/shirt.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	style: DS.belongsTo('shirtstyle'),
-	size: DS.belongsTo('shirtsize'),
-	color: DS.belongsTo('shirtcolor'),
-	material: DS.belongsTo('shirtmaterial')
+	style: DS.attr(),
+	size: DS.attr(),
+	color: DS.attr(),
+	material: DS.attr()
 });
--- web/app/routes/shirts/list.js
+++ web/app/routes/shirts/list.js
@@ -2,8 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    this.store.adapterFor('application').set('host', 'http://catalog.peoplerunning.co');
-    debugger;
+    this.store.adapterFor('application').set('host', 'http://catalog.peoplerunning.co');    
     return this.get('store').findAll('shirt');
   }
 });
