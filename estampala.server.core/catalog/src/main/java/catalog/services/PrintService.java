b7c3666eb8788fb6ffdde00ef626c79fc154809a
JarveinR (akane) <jarvein.rivera@bizagi.com>
2017-05-08 13:25:19 -0500

    Variability and other bugs fixes

--- estampala.server.core/catalog/src/main/java/catalog/services/PrintService.java
+++ estampala.server.core/catalog/src/main/java/catalog/services/PrintService.java
@@ -165,7 +165,7 @@ public class PrintService {
 			ratePrint.setRate(rate);
 		}
 		else{
-			print.setRating((rating + rate) / 2);
+			print.setRating(((rating * counts) + rate) / (counts + 1));
 			print.setRatingCounts(counts + 1);
 			ratePrint = new RatePrint(UUID.randomUUID(), UUID.fromString(idUser), idPrint, rate);
 		}		
