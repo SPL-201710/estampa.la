a08db4b5d82dc8713f333cce902db9cac5ad5b45
Jorge Perea <george5015@gmail.com>
2017-05-20 19:16:00 -0500

    Refactoring of payment methods

--- estampala.server.core/payment/src/main/java/payment/models/PaymentMethodCreditCard.java
+++ estampala.server.core/payment/src/main/java/payment/models/PaymentMethodCreditCard.java
@@ -1,11 +1,42 @@
 package payment.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
 public class PaymentMethodCreditCard extends PaymentMethod{
+	
+	@Column(nullable = true)
+	private String firtsName;
 
+	@Column(nullable = true)
+	private String lastName;
+
+	@Column(nullable = true)
+	private String phone;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String bank;
+
+	@Column(nullable = false)
+	private String bankCode;
+
+	@Column(nullable = false)
+	private String reference;
+
+	@Column(nullable = false)
+	private String identificationType;
+
+	@Column(nullable = false)
+	private String identification;
+	
+	@Column(nullable = false)
 	private String cardNumber;
+	
+	@Column(nullable = false)
 	private String cvc;
 	
 	public String getCardNumber() {
