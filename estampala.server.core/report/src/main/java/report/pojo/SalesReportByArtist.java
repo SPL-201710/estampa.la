c35a6d850ba361531ae47de1f8c8b823de317bc6
akane <jarvein@hotmail.com>
2017-05-21 11:41:19 -0500

    Mock salesByArtist

--- estampala.server.core/report/src/main/java/report/pojo/SalesReportByArtist.java
+++ estampala.server.core/report/src/main/java/report/pojo/SalesReportByArtist.java
@@ -1,5 +1,44 @@
 package report.pojo;
 
-public class SalesReportByArtist {
+import java.util.UUID;
 
+public class SalesReportByArtist {
+		
+	private String printName;
+	private UUID printId;
+	private String printImage;
+	private int quantitySold;
+	private double totalSold;
+	
+	
+	public String getPrintName() {
+		return printName;
+	}
+	public void setPrintName(String printName) {
+		this.printName = printName;
+	}
+	public UUID getPrintId() {
+		return printId;
+	}
+	public void setPrintId(UUID printId) {
+		this.printId = printId;
+	}
+	public String getPrintImage() {
+		return printImage;
+	}
+	public void setPrintImage(String printImage) {
+		this.printImage = printImage;
+	}
+	public int getQuantitySold() {
+		return quantitySold;
+	}
+	public void setQuantitySold(int quantitySold) {
+		this.quantitySold = quantitySold;
+	}
+	public double getTotalSold() {
+		return totalSold;
+	}
+	public void setTotalSold(double totalSold) {
+		this.totalSold = totalSold;
+	}	
 }
