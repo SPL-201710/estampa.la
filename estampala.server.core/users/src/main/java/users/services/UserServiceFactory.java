2f5e09e997d6d24d487e77dd996fee87e26a81d8
Jorge Perea <george5015@gmail.com>
2017-05-21 20:50:15 -0500

    Added variable over constant condition for social login, credit card and giftcard

--- estampala.server.core/users/src/main/java/users/services/UserServiceFactory.java
+++ estampala.server.core/users/src/main/java/users/services/UserServiceFactory.java
@@ -3,6 +3,9 @@ package users.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import commons.exceptions.ResourceNotFoundException;
+import commons.util.FeaturesFlag;
+
 @Service
 public class UserServiceFactory {
 
@@ -12,15 +15,19 @@ public class UserServiceFactory {
 	@Autowired
 	private UserServiceFacebook userServiceFacebook;
 	
-	public UserService getInstance(String authenticationMethod){
+	public UserService getInstance(String authenticationMethod) throws ResourceNotFoundException{
 		return getInstance(AuthenticationMethods.parse(authenticationMethod));
 	}
 	
-	public UserService getInstance(AuthenticationMethods authenticationMethod){
+	public UserService getInstance(AuthenticationMethods authenticationMethod) throws ResourceNotFoundException{
 		
 		switch(authenticationMethod){
 			case SYSTEM: return userServiceSystem;						
-			case FACEBOOK: return userServiceFacebook;				
+			case FACEBOOK: if(!FeaturesFlag.AUTH_FACEBOOK.isActive()) {
+				throw new ResourceNotFoundException("auth_facebook");
+			}
+				return userServiceFacebook;				
+			
 			default: return userServiceSystem;
 		}
 	}
