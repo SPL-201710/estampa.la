68c4e6633c5bba34c972b73ef12553f20129d87d
soybackend <josealvarez712@gmail.com>
2017-05-22 22:06:25 -0500

    facebook login frontend ok

---
+++ web/app/authorizers/facebook.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import Base from 'simple-auth/authorizers/base';
+
+export default Base.extend({
+  authorize: function(jqXHR, requestOptions) {
+    debugger;
+    var accessToken = this.get('session.content.secure.token');
+    if (this.get('session.isAuthenticated') && !Ember.isEmpty(accessToken)) {
+      jqXHR.setRequestHeader('Authorization', 'Token ' + accessToken);
+    }
+  }
+});
